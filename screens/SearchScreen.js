9eb829e3c3d172c1a7487fb84d3ee377db5693aa
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-10-11 05:56:35 -0500

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -5,7 +5,7 @@ export default class Searchscreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Search</Text>
+          <Text>Buscar</Text>
         </View>
       );
     }
